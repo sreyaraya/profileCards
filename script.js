b22b4a55658cd17994cc2c394888cabc224d0bde
sreyaraya <sreyarayaprolu@gmail.com>
2023-07-18 12:39:00 -0700

    update

---
+++ script.js
@@ -0,0 +1,32 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+//if this isn't working, try updating the octoki token: 
+//https://github.com/settings/tokens/new?scopes=repo
+//mport config from "../config.json";
+//const Octokit = require("@octokit/core").Octokit
+//import {Octokit} from "https://esm.sh/@octokit/rest";
+//const octokit = new Octokit({
+//})
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+confetti();
+
+//const octokit = new Octokit({
+//    auth: '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'
+ // })
+  
+ // await octokit.request('GET /user', {
+   // headers: {
+     // 'X-GitHub-Api-Version': '2022-11-28'
+    //}
+  //})
+
+//getUser()
+async function getUser(){
+    console.log('calling');
+    //const result = await resolveAfter2Seconds();
+    //console.log(result);
+  // Expected output: "resolved"
+
+}
+//rest api
+//async model
